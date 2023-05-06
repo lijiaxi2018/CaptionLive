689b93cc3e000cf6fff7d79ff3bee69d11ef57d3
liulin <liulin0x3c@foxmail.com>
2023-05-07 00:53:55 +0800

    init

---
+++ src/main/java/com/example/ll/model/Ownership.java
@@ -0,0 +1,20 @@
+package com.example.ll.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ownerships")
+public class Ownership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ownershipId;
+
+    private Long projectId;
+
+    private Long organizationId;
+
+}
