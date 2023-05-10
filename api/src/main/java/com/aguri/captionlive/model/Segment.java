48631418d5856bad2954b3087c76a24dc0afaf7a
liulin <liulin0x3c@foxmail.com>
2023-05-11 05:29:45 +0800

    <chore: formatting>

--- api/src/main/java/com/aguri/captionlive/model/Segment.java
+++ api/src/main/java/com/aguri/captionlive/model/Segment.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
--- api/src/main/java/com/aguri/captionlive/model/Task.java
+++ api/src/main/java/com/aguri/captionlive/model/Task.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
