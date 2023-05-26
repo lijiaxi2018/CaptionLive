2772b02afa47f6d972ef0e63c542f60f21f7e51e
liulin <liulin0x3c@foxmail.com>
2023-05-27 02:47:05 +0800

    <chore: typo>

--- api/src/main/java/com/aguri/captionlive/controller/UserController.java
+++ api/src/main/java/com/aguri/captionlive/controller/UserController.java
@@ -10,20 +10,13 @@ import com.aguri.captionlive.service.FileRecordService;
 import com.aguri.captionlive.service.ProjectService;
 import com.aguri.captionlive.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
