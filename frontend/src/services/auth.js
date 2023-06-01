ef2f474b8ded51a52c5daa2b8bacd3299cd90131
Jiaxi Li <ljx@wirelessprv-10-195-70-26.near.illinois.edu>
2023-05-31 19:04:03 -0500

    feat: frontend added signin api

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/api/' }),
 
   tagTypes: ['Auth'],
   endpoints: (builder) => ({
