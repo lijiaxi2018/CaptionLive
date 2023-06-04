751046d8ee192170102638ca5bfecb36b451a02b
liulin <liulin0x3c@foxmail.com>
2023-06-05 05:47:27 +0800

    <chore: typo>

--- api/src/main/java/com/aguri/captionlive/model/Project.java
+++ api/src/main/java/com/aguri/captionlive/model/Project.java
@@ -2,7 +2,6 @@ package com.aguri.captionlive.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
