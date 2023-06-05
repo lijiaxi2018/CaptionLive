85ab2135e94a0a81163462396e8dfaee694ccc68
Jiaxi Li <ljx@MacBook-Pro-672.local>
2023-06-04 23:49:34 -0500

    chore: api rename init database

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (permission, username, password, qq, email, description)
-VALUES (0, 'Uehara', 'password', 'qq1', 'email1', 'description1'),
-       (0, 'Takasaki', 'password', 'qq2', 'email2', 'description2')
+VALUES (0, '上原步梦', 'password', 'qq1', 'email1', 'description1'),
+       (0, '高咲侑', 'password', 'qq2', 'email2', 'description2')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO organizations (name, description)
-VALUES ('Nijigasaki', 'description'),
-       ('Uranohoshi', 'description')
+VALUES ('虹之咲字幕组', 'description'),
+       ('浦之星字幕组', 'description')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO memberships (permission, position, organization_id, user_id)
