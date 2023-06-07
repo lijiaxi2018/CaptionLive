1db043a7e9c6d7185b7d9c3570149eff2baa0e3a
Jiaxi Li <jiaxili@MacBook-14.local>
2023-06-07 03:07:17 -0500

    feat: changed url direct; added selectedOrgId to sessionStorage

--- frontend/src/redux/layoutSlice.js
+++ frontend/src/redux/layoutSlice.js
@@ -1,9 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const sessionSelectedOrgId = sessionStorage.getItem("clSelectedOrgId") !== null ? JSON.parse(sessionStorage.getItem("clSelectedOrgId")) : -1;
+
 const initialState = {
   openSignInOnWindow: false,
   inSignIn: false,
   inEditUser: false,
+  selectedOrgId: sessionSelectedOrgId,
 }
 
 export const layoutSlice = createSlice({
@@ -21,9 +24,14 @@ export const layoutSlice = createSlice({
     toggleInEditUser: (state) => {
       state.inEditUser = !state.inEditUser;
     },
+
+    updateSelectedOrgId: (state, action) => {
+      state.selectedOrgId = action.payload;
+      sessionStorage.setItem("clSelectedOrgId", JSON.stringify(state.selectedOrgId));
+    },
   },
 })
 
-export const { toggleSignInOnWindow, toggleSignInOnPage, toggleInEditUser } = layoutSlice.actions
+export const { toggleSignInOnWindow, toggleSignInOnPage, toggleInEditUser, updateSelectedOrgId } = layoutSlice.actions
 
 export default layoutSlice.reducer
