d50311679783a43f172c68ad533d0cc8bee7188f
Jiaxi Li <ljx@MacBook-Pro-672.local>
2023-06-09 00:39:14 -0500

    chore: updated init data

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -13,3 +13,48 @@ VALUES (1, 0, 1, 1),
        (0, 0, 1, 2),
        (0, 0, 2, 1)
 ON CONFLICT DO NOTHING;
+
+INSERT INTO projects (is_public, name, type)
+VALUES (false, '虹5th Day4', 0)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO ownerships (organization_id, project_id)
+VALUES (1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO accesses (commitment, permission, user_id, project_id)
+VALUES (0, 0, 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO segments (is_global, project_id)
+VALUES (true, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO tasks (status, type, segment_id)
+VALUES (0, 8, 1),
+       (0, 9, 1),
+       (0, 10, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO segments (is_global, scope, summary, project_id)
+VALUES (false, '0:1800', '全篇', 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO tasks (status, type, segment_id)
+VALUES (0, 0, 2)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO tasks (status, type, segment_id, worker_user_id)
+VALUES (1, 3, 2, 1),
+       (1, 7, 2, 2)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO segments (is_global, scope, summary, project_id)
+VALUES (false, '1800:3600', '返场', 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO tasks (status, type, segment_id)
+VALUES (0, 0, 3),
+       (0, 3, 3),
+       (0, 7, 3)
+ON CONFLICT DO NOTHING;
