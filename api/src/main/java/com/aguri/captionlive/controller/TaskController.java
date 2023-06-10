b7a5c35223c8a65488890fca03b43d8f7b9045f6
liulin <liulin0x3c@foxmail.com>
2023-06-10 12:16:01 +0800

    <fix: add annotation>

--- api/src/main/java/com/aguri/captionlive/controller/TaskController.java
+++ api/src/main/java/com/aguri/captionlive/controller/TaskController.java
@@ -4,6 +4,11 @@ import com.aguri.captionlive.common.resp.Resp;
 import com.aguri.captionlive.model.Task;
 import com.aguri.captionlive.service.FileRecordService;
 import com.aguri.captionlive.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -55,20 +60,41 @@ public class TaskController {
 //    }
 
     @PutMapping("/{taskId}/assign/{userId}")
-    public ResponseEntity<Resp> assign(@PathVariable Long taskId, @PathVariable Long userId) {
+    @Operation(summary = "Assign task to user", description = "Assign a task to a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task assigned successfully",
+                    content = @Content(schema = @Schema(implementation = Resp.class)))
+    })
+    public ResponseEntity<Resp> assign(
+            @PathVariable("taskId") Long taskId,
+            @PathVariable("userId") Long userId) {
         Task updatedTask = taskService.assign(taskId, userId);
         return ResponseEntity.ok(Resp.ok(updatedTask));
     }
 
     @DeleteMapping("/{taskId}/assign")
-    public ResponseEntity<Resp> withdrawalAssign(@PathVariable Long taskId) {
+    @Operation(summary = "Withdraw assignment", description = "Withdraw the assignment of a task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Assignment withdrawal successful",
+                    content = @Content(schema = @Schema(implementation = Resp.class)))
+    })
+    public ResponseEntity<Resp> withdrawalAssign(
+            @PathVariable("taskId") Long taskId) {
         Task updatedTask = taskService.withdrawalAssign(taskId);
         return ResponseEntity.ok(Resp.ok(updatedTask));
     }
 
     @PutMapping("/{taskId}/file/{fileRecordId}")
-    public ResponseEntity<Resp> uploadFileAndTaskStatusChange(@PathVariable Long taskId, @PathVariable Long fileRecordId) {
+    @Operation(summary = "Upload file and update task status", description = "Upload a file and update the task status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File uploaded and task status changed successfully",
+                    content = @Content(schema = @Schema(implementation = Resp.class)))
+    })
+    public ResponseEntity<Resp> uploadFileAndTaskStatusChange(
+            @PathVariable("taskId") Long taskId,
+            @PathVariable("fileRecordId") Long fileRecordId) {
         return ResponseEntity.ok(Resp.ok(taskService.uploadFileAndTaskStatusChange(taskId, fileRecordId)));
     }
+
 
 }
