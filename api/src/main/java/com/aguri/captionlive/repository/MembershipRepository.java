3e135b8dcae2fe2084dc244117c94b90d1908ba6
liulin <liulin0x3c@foxmail.com>
2023-06-21 22:21:25 +0800

    <feat: add new property 'leaderIds' in get organization by id resp>

--- api/src/main/java/com/aguri/captionlive/repository/MembershipRepository.java
+++ api/src/main/java/com/aguri/captionlive/repository/MembershipRepository.java
@@ -4,9 +4,11 @@ import com.aguri.captionlive.model.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership,Long> {
+    List<Membership> findAllByOrganizationOrganizationIdAndPositionIn(Long id, Collection<Membership.Position> position);
 }
