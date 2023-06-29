16b024cef3a64b47dc126b0cffbb2037fe08d8c0
liulin <liulin0x3c@foxmail.com>
2023-06-29 11:29:55 +0800

    <feat: open switch for jwtFilter>

--- api/src/main/java/com/aguri/captionlive/common/filter/JwtAuthenticationFilter.java
+++ api/src/main/java/com/aguri/captionlive/common/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @WebFilter(urlPatterns = "/*")
@@ -19,33 +21,47 @@ public class JwtAuthenticationFilter implements Filter {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    private static final List<String> EXCLUDED_URLS = Arrays.asList("/api/login", "/api/signUp");
     private static final String LOGIN_PATH = "/login";
-    private static final String API_PATH_PREFIX = "/api/";
+
+    private static final Boolean OPEN = Boolean.TRUE;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        String requestURI = request.getRequestURI();
-        if (requestURI.startsWith(API_PATH_PREFIX)) {
-            // 获取请求中的JWT
-            String token = request.getHeader("Authorization");
+        if (OPEN) {
 
-            if (token != null && jwtTokenProvider.validateToken(token)) {
-                // 用户已登录，验证JWT通过
-                request.setAttribute("username", jwtTokenProvider.parseUserNameFromToken(token));
-            } else {
-                // 用户未登录或JWT验证失败
+            String requestURI = request.getRequestURI();
+            if (!isExcludedUrl(requestURI)) {
+                // 获取请求中的JWT
+                String token = request.getHeader("Authorization");
+
+                if (token != null && jwtTokenProvider.validateToken(token)) {
+                    // 用户已登录，验证JWT通过
+                    request.setAttribute("username", jwtTokenProvider.parseUserNameFromToken(token));
+                } else {
+                    // 用户未登录或JWT验证失败
 //                response.sendRedirect(LOGIN_PATH);
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
             }
         }
 
 
         // 将请求传递给下一个过滤器或目标资源
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isExcludedUrl(String requestURI) {
+        for (String excludedUrl : EXCLUDED_URLS) {
+            if (requestURI.startsWith(excludedUrl)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
