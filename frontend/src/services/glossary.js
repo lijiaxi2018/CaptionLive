7875c182560ec2717b2f1d5cb7604c2823e84c71
Jiaxi Li <ljx@wirelessprv-10-195-70-26.near.illinois.edu>
2023-07-08 17:48:13 -0500

    chore: added header for glossary service

--- frontend/src/services/glossary.js
+++ frontend/src/services/glossary.js
@@ -1,8 +1,18 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { configuration } from '../config/config';
 
 export const glossaryApi = createApi({
     reducerPath: 'glossaryApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/api/' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: `http://${configuration.HOSTNAME}:8080/api/`,
+        prepareHeaders: (headers) => {
+        const userToken = JSON.parse(localStorage.getItem("clAccessToken"));
+        if (userToken) {
+            headers.set('Authorization', userToken);
+        }
+        return headers
+        },
+    }),
 
     tagTypes: ['Glossaries'],
     endpoints: (builder) => ({
